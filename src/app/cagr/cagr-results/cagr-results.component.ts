b1e1fbd842f071d0190a9cd3f2636b1b060238e7
Kumar Gandhi Koppolu <kumargandhi30@gmail.com>
2022-02-18 13:18:35 +0530

    PDF Export in carg

--- src/app/cagr/cagr-results/cagr-results.component.ts
+++ src/app/cagr/cagr-results/cagr-results.component.ts
@@ -71,5 +71,46 @@ export class CagrResultsComponent implements OnInit {
         this.resultsCalculated = true;
     }
 
-    exportToPDF() {}
+    exportToPDF() {
+        // Creating a unique file name for my PDF
+        const fileName =
+            this.title.replace(' ', '_') +
+            '_' +
+            Math.floor(Math.random() * 1000000 + 1) +
+            '.pdf';
+        const doc = new jsPDF();
+        doc.setFontSize(20);
+        doc.setFont('helvetica', 'bold');
+        const titleXPos =
+            doc.internal.pageSize.getWidth() / 2 -
+            doc.getTextWidth(this.title) / 2;
+        doc.text(this.title, titleXPos, 20);
+        doc.setFont('helvetica', 'normal');
+        doc.setFontSize(14);
+        doc.text(this._getLoanDetails(), 10, 30);
+        doc.save(fileName);
+    }
+
+    private _getLoanDetails() {
+        let loanDetails = '';
+        loanDetails =
+            'Purchase item details:' +
+            '\n\n' +
+            'Purchase value: ' +
+            this.nf.transform(this._calculatorInputs.purchaseValue);
+        loanDetails =
+            loanDetails +
+            '\n' +
+            'Present value: ' +
+            this.nf.transform(this._calculatorInputs.presentValue);
+        loanDetails =
+            loanDetails + '\n' + 'Years: ' + this._calculatorInputs.years;
+        loanDetails = loanDetails + '\n\n' + 'Summary:';
+        loanDetails =
+            loanDetails +
+            '\n\n' +
+            this.nf.transform(this.computedValues.cagr) +
+            " - is 'Compound Annualised Growth Rate'.";
+        return loanDetails;
+    }
 }
